641e3ba4aee25439ee0d1de8fcaf23fa878d59a4
Rashad Mahmudov <mkrashad25@gmail.com>
2019-11-12 11:37:28 +0100

    Implementing TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import * as React from "react";
+import ButtonItem from "./ButtonItem";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello!</h1>
-      </div>
-    );
-  }
-}
+const App: React.FC = props => {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div>
+      <div onClick={() => setCount(count + 1)}>{count}</div>
+      <button id="equals">=</button>
+      <ButtonItem />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/ButtonItem.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const ButtonItem: React.FC = props => {
+  return <div>hey</div>;
+};
+
+export default ButtonItem;
