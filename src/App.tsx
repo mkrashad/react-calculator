c596400a77a8ae571de8ee4ff2fb39a622d38e56
Rashad <mkrashad25@gmail.com>
2020-07-27 23:12:44 +0200

    Created scss file

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import * as React from "react";
 import { render } from "react-dom";
 import { StoreProvider } from "easy-peasy";
+import store from './store';
+import '../public/App.scss';
 import Numbers from "./components/Numbers";
 import Operators from "./components/Operators";
 import Display from "./components/Display";
-import store from './store';
 
 
 const App: React.FC = () => {
 
   return (
     <StoreProvider store={store}>
-      <div style={{ marginLeft: 500 }}>
+      <div className="wrapper">
         <Numbers />
         <Operators />
         <Display />
-      </div>
+      </div >
     </StoreProvider>
   );
 };
